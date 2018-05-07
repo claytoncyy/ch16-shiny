ebee014a823f2746b570645ba15d37fb17ee2cf8
Yangyu Chen <cheny289@uw.edu>
2018-05-07 16:52:12 -0700

    finished

--- exercise-2/app.R
+++ exercise-2/app.R
@@ -5,23 +5,27 @@ library(shiny)
 
 # Define a new `ui` variable. This variable should be assigned a `fluidPage()` layout
 # The `fluidPage()` layout should be passed the following:
+  ui <- fluidPage(titlePanel("Cost Calculator"),
+                  numericInput("price", label = "Pirce", value = 0, min = 0),
+                  numericInput("quantity", label = "Quantity", value = 1, min = 1),
+                  strong("cost"),
+                  textOutput("cost"))
 
-  
-  # A `titlePanel()` layout with the text "Cost Calculator"
+    # A `titlePanel()` layout with the text "Cost Calculator"
 
-  
-  # A `numericInput()` widget with the label "Price (in dollars)"
-  # It should have a default value of 0 and a minimum value of 0
-  # Hint: look up the function's arguments in the documentation!
 
-  
-  # A second `numericInput()` widget with the label "Quantity"
-  # It should have a default value of 1 and a minimum value of 1  
+    # A `numericInput()` widget with the label "Price (in dollars)"
+    # It should have a default value of 0 and a minimum value of 0
+    # Hint: look up the function's arguments in the documentation!
 
-  
+
+    # A second `numericInput()` widget with the label "Quantity"
+    # It should have a default value of 1 and a minimum value of 1
+
+
   # The word "Cost", strongly bolded
 
-  
+
   # A `textOutput()` output of a calculated value labeled `cost`
 
 
@@ -29,11 +33,17 @@ library(shiny)
 
 # Define a `server` function (with appropriate arguments)
 # This function should perform the following:
+server <- function(input, output) {
+  output$cost <- renderText({
 
-  
+    return(paste0("$", input$price * input$quantity))
+
+  })
+}
+
   # Assign a reactive `renderText()` function to the output's `cost` value
   # The reactive expression should return the input `price` times the `quantity`
   # So it looks nice, paste a "$" in front of it!
 
-
 # Create a new `shinyApp()` using the above ui and server
+shinyApp(ui = ui, server = server)
