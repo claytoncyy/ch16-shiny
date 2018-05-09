6d6bfeb85eae99e3e7efdb114eeedb5acb7ae8ea
Yangyu Chen <cheny289@uw.edu>
2018-05-09 16:04:22 -0700

    finished

--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -5,13 +5,19 @@ library(shiny)
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
-
+  shinyUI(fluidPage(h1("header"),
+                   selectInput("x_var", label = "X", choices = select_values, selected = "displ"),
+                   selectInput("y_var", label = "Y", choices = select_values, selected = "cyl"),
+                   sliderInput("size", label = "size", min = 1, max = 10, value = 5),
+                   selectInput("color", label = "color",
+                               choices = list("Red" = "red", "Blue" = "blue", "Green" = "green")),
+                   plotOutput("scatter")))
   # A page header
 
-  
+
   # Add a select input for the x variable
 
-  
+
   # Add a select input for the y variable
 
 
